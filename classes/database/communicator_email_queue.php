66004e5e82c8599c09122561aae3757224dca85d
Buddhimal Gunasekara <bnawgunasekara@gmail.com>
2020-07-07 15:07:16 +0530

    add receiver_name

--- classes/database/communicator_email_queue.php
+++ classes/database/communicator_email_queue.php
@@ -11,7 +11,7 @@ class communicator_email_queue {
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 			/* We suppose the "errors" table contains the errors we look for */
-			$sql = sprintf("SELECT id,sender_name,sender_email,email_type_id,send_to,template_id,content,delivery_status,delivered, error,is_deleted,is_active,updated,created,updated_by,created_by FROM communicator_email_queue WHERE delivery_status = %d", EmailStatus::Pending);
+			$sql = sprintf("SELECT id,sender_name,sender_email,email_type_id,receiver_name,send_to,template_id,content,delivery_status,delivered, error,is_deleted,is_active,updated,created,updated_by,created_by FROM communicator_email_queue WHERE delivery_status = %d", EmailStatus::Pending);
 
 			$st = $db->prepare($sql);
 			$st->execute();
