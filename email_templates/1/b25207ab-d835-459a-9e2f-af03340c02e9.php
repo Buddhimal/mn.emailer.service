4aab062e4b7e4f15dd571d1ec71dd457fbadee97
Asanka De Silva <easankad@gmail.com>
2020-07-07 17:09:02 +0530

    Sending email successfully. :) still need some improvements. need to add logging.

--- email_templates/1/b25207ab-d835-459a-9e2f-af03340c02e9.php
+++ email_templates/1/b25207ab-d835-459a-9e2f-af03340c02e9.php
@@ -5,48 +5,50 @@
 	<title>Welcome on board</title>
 </head>
 <body style="background-color: #aeaeae; text-align: center; padding: 0px; margin: 0px; font-family: sans-serif; font-size: 11pt; color: #6b6b6b;">
-	<div style="margin-left: auto; margin-right: auto; background-color: #ffffff; width: 800px; padding: 50px 20px 10px 20px; margin-top:10px;">
-		
-		<div style="text-align: center; margin-bottom: 30px;">
-			<div style="width: 100px; margin-left: auto; margin-right: auto; border-bottom: 2px solid #efefef;padding-bottom: 5px;">
-				<img src="<?php echo trim(CDN_URL, "/"); ?>/email_templates/logo.png" width="100" />
+	<div style=" margin-top:10px;">
+		<div style="margin-left: auto; margin-right: auto; background-color: #ffffff; width: 800px; padding: 50px 20px 10px 20px; margin-top:10px;">
+			
+			<div style="text-align: center; margin-bottom: 30px;">
+				<div style="width: 100px; margin-left: auto; margin-right: auto; border-bottom: 2px solid #efefef;padding-bottom: 5px;">
+					<img src="<?php echo trim(CDN_URL, "/"); ?>/email_templates/logo.png" width="100" />
+				</div>
+			</div>
+
+			<p style="display: block; text-align: left;">Hi ##NAME##</p>
+
+			<h4 style="font-size: 1.6em;">Welcome onboard</h4>
+			<div style="line-height: 1.5em; margin-top: 20px; margin-bottom:20px;">
+				You have been successfully signed up and registered with us. Now you are all set to make your first appointment to your family doctor.
+			</div>
+			<div  style="line-height: 1.5em; margin-top: 20px; margin-bottom:20px;">
+				Wish you a healthy life, enjoy the social distancing with this new revolutionary app.
+			</div>
+
+		</div>
+
+		<div style="margin-left: auto; margin-right: auto; background-color: #f2f2f2; width: 800px; padding: 30px 20px 60px 20px;">
+			<h4 style="font-size: 1.6em;">Verify Your Email</h4>
+			<div>
+				<a href="##EMAILVERIFICATION_URL##" alt="" style="color:#ff2a2a; font-weight: bold;">Click here to verify your email and activate your account</a>
 			</div>
 		</div>
 
-		<p style="display: block; text-align: left;">Hi ##NAME##</p>
-
-		<h4 style="font-size: 1.6em;">Welcome onboard</h4>
-		<div style="line-height: 1.5em; margin-top: 20px; margin-bottom:20px;">
-			You have been successfully signed up and registered with us. Now you are all set to make your first appointment to your family doctor.
-		</div>
-		<div  style="line-height: 1.5em; margin-top: 20px; margin-bottom:20px;">
-			Wish you a healthy life, enjoy the social distancing with this new revolutionary app.
+		<div style="margin-left: auto; margin-right: auto; background-color: #ffffff; width: 800px; padding: 20px 20px 70px 20px;">
+			<h4 style="font-size: 1.6em;">Need Help ?</h4>
+			<div>
+				We are happy to assist you at any time. Our representatives are available from 8:00am to 10:00pm, 7 days a week.
+			</div>
 		</div>
 
-	</div>
-
-	<div style="margin-left: auto; margin-right: auto; background-color: #f2f2f2; width: 800px; padding: 30px 20px 60px 20px;">
-		<h4 style="font-size: 1.6em;">Verify Your Email</h4>
-		<div>
-			<a href="##EMAILVERIFICATION_URL##" alt="" style="color:#ff2a2a; font-weight: bold;">Click here to verify your email and activate your account</a>
+		<div style="margin-left: auto; margin-right: auto; width: 800px; padding: 10px; font-size: 0.8em;">
+			<p style="margin: 0px; padding: 0px; line-height: 1.2em;">Softgen International Pvt Ltd. | #75/4, Colombo 07.</p>
+			<p style="margin: 0px; padding: 0px; line-height: 1.2em;">www.mynumber.lk | info@mynumber.lk</p>
+			<p style="margin: 0px; padding: 0px; line-height: 1.2em;">
+				<a href="http://www.mynumber.lk" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">HOME</a> | 
+				<a href="http://www.mynumber.lk/#product" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">ABOUT</a> | 
+				<a href="http://www.mynumber.lk/#contact" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">CONTACTS</a>
+			</p>
 		</div>
-	</div>
-
-	<div style="margin-left: auto; margin-right: auto; background-color: #ffffff; width: 800px; padding: 20px 20px 70px 20px;">
-		<h4 style="font-size: 1.6em;">Need Help ?</h4>
-		<div>
-			We are happy to assist you at any time. Our representatives are available from 8:00am to 10:00pm, 7 days a week.
-		</div>
-	</div>
-
-	<div style="margin-left: auto; margin-right: auto; width: 800px; padding: 10px; font-size: 0.8em;">
-		<p style="margin: 0px; padding: 0px; line-height: 1.2em;">Softgen International Pvt Ltd. | #75/4, Colombo 07.</p>
-		<p style="margin: 0px; padding: 0px; line-height: 1.2em;">www.mynumber.lk | info@mynumber.lk</p>
-		<p style="margin: 0px; padding: 0px; line-height: 1.2em;">
-			<a href="http://www.mynumber.lk" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">HOME</a> | 
-			<a href="http://www.mynumber.lk/#product" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">ABOUT</a> | 
-			<a href="http://www.mynumber.lk/#contact" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">CONTACTS</a>
-		</p>
 	</div>
 </body>
 </html>
--- email_templates/1/c38d8e35-5161-4970-9ae2-d0f7c00bf9d2.php
+++ email_templates/1/c38d8e35-5161-4970-9ae2-d0f7c00bf9d2.php
@@ -4,45 +4,47 @@
 	<title>Welcome on board</title>
 </head>
 <body style="background-color: #aeaeae; text-align: justify; padding: 0px; margin: 0px; font-family: sans-serif; font-size: 11pt; color: #6b6b6b;">
-	<div style="margin-left: auto; margin-right: auto; background-color: #ffffff; width: 500px; padding: 50px 100px; margin-top:10px;">
-		
-		<div style="text-align: center; margin-bottom: 30px;">
-			<div style="width: 100px; margin-left: auto; margin-right: auto; border-bottom: 2px solid #efefef;padding-bottom: 5px;">
-				<img src="<?php echo trim(CDN_URL, "/"); ?>/email_templates/logo.png" width="100" />
+	<div style=" margin-top:10px;">
+		<div style="margin-left: auto; margin-right: auto; background-color: #ffffff; width: 500px; padding: 50px 100px; margin-top:10px;">
+			
+			<div style="text-align: center; margin-bottom: 30px;">
+				<div style="width: 100px; margin-left: auto; margin-right: auto; border-bottom: 2px solid #efefef;padding-bottom: 5px;">
+					<img src="<?php echo trim(CDN_URL, "/"); ?>/email_templates/logo.png" width="100" />
+				</div>
+			</div>
+
+			<p style="display: block; text-align: left;">Hi ##NAME##</p>		
+			<div style="line-height: 1.5em; margin-top: 20px; margin-bottom:20px;">
+
+				<p>I noticed that you recently signed up with us and I wanted to check in with you to ensure that you have everything you need. I hope that your experience with mynumber so far has been a pleasant one.</p>
+
+				<p>Here at mynumber customer experience is at the heart of everything we do. It's why we come to work each day. All replies to this email inbox are monitored by myself, so if you'd like to get in touch directly and provide any feedback which could help us help you, please hit reply and I'll ensure that we get onto that right away. No issue is too small. If it matters to you, it matters to us, so please do get in touch if you need to.</p>
+
+				<p>
+				Also, don't forget that our rockstar customer support team is here for all your day-to-day and technical questions 24/7.</p>
+
+				<p>
+				And finally, we totally get that there's nothing on the internet more intimate than your inbox, so we never bombard our customers with unwanted emails or in-your-face advertising. We respect that wholeheartedly. That's why we give you the power to control your email notifications directly from your account.</p>
+
+				<p>
+				Thanks again and I'm delighted to have you on board and look forward to helping you drive your business to awesome new heights.</p>
+
+				<p>
+				Gratefully yours,<br/><br/>			
+				Sampath De Soyza<br/>
+				CEO, mynumber llc</p>
 			</div>
 		</div>
 
-		<p style="display: block; text-align: left;">Hi ##NAME##</p>		
-		<div style="line-height: 1.5em; margin-top: 20px; margin-bottom:20px;">
-
-			<p>I noticed that you recently signed up with us and I wanted to check in with you to ensure that you have everything you need. I hope that your experience with mynumber so far has been a pleasant one.</p>
-
-			<p>Here at mynumber customer experience is at the heart of everything we do. It's why we come to work each day. All replies to this email inbox are monitored by myself, so if you'd like to get in touch directly and provide any feedback which could help us help you, please hit reply and I'll ensure that we get onto that right away. No issue is too small. If it matters to you, it matters to us, so please do get in touch if you need to.</p>
-
-			<p>
-			Also, don't forget that our rockstar customer support team is here for all your day-to-day and technical questions 24/7.</p>
-
-			<p>
-			And finally, we totally get that there's nothing on the internet more intimate than your inbox, so we never bombard our customers with unwanted emails or in-your-face advertising. We respect that wholeheartedly. That's why we give you the power to control your email notifications directly from your account.</p>
-
-			<p>
-			Thanks again and I'm delighted to have you on board and look forward to helping you drive your business to awesome new heights.</p>
-
-			<p>
-			Gratefully yours,<br/><br/>			
-			Sampath De Soyza<br/>
-			CEO, mynumber llc</p>
+		<div style="margin-left: auto; margin-right: auto; width: 700px; padding: 10px; font-size: 0.8em; text-align: center;">
+			<p style="margin: 0px; padding: 0px; line-height: 1.2em;">Softgen International Pvt Ltd. | #75/4, Colombo 07.</p>
+			<p style="margin: 0px; padding: 0px; line-height: 1.2em;">www.mynumber.lk | info@mynumber.lk</p>
+			<p style="margin: 0px; padding: 0px; line-height: 1.2em;">
+				<a href="http://www.mynumber.lk" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">HOME</a> | 
+				<a href="http://www.mynumber.lk/#product" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">ABOUT</a> | 
+				<a href="http://www.mynumber.lk/#contact" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">CONTACTS</a>
+			</p>
 		</div>
-	</div>
-
-	<div style="margin-left: auto; margin-right: auto; width: 700px; padding: 10px; font-size: 0.8em; text-align: center;">
-		<p style="margin: 0px; padding: 0px; line-height: 1.2em;">Softgen International Pvt Ltd. | #75/4, Colombo 07.</p>
-		<p style="margin: 0px; padding: 0px; line-height: 1.2em;">www.mynumber.lk | info@mynumber.lk</p>
-		<p style="margin: 0px; padding: 0px; line-height: 1.2em;">
-			<a href="http://www.mynumber.lk" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">HOME</a> | 
-			<a href="http://www.mynumber.lk/#product" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">ABOUT</a> | 
-			<a href="http://www.mynumber.lk/#contact" target="_blank;" style="color: #4e4e4e; font-size: 0.8em;">CONTACTS</a>
-		</p>
 	</div>
 </body>
 </html>
